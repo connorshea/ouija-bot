ca29c71d2205b3bea0eb889dfb2234c1db7ac13a
Connor Shea <connor.james.shea@gmail.com>
2018-06-07 16:38:46 -0600

    Add VS Code tasks.json and improve the ping command.

--- src/modules/commands/ping.rb
+++ src/modules/commands/ping.rb
@@ -3,8 +3,11 @@ module Bot::DiscordCommands
   # This used to check if bot is alive
   module Ping
     extend Discordrb::Commands::CommandContainer
-    command :ping do |_event|
-      'Pong!'
+    command(:ping, description: "Pings the bot, the bot will return a time in milliseconds to show how long the response took.") do |_event|
+      # The `respond` method returns a `Message` object, which is stored in a variable `m`. The `edit` method is then called
+      # to edit the message with the time difference between when the event was received and after the message was sent.
+      m = event.respond('Pong!')
+      m.edit "Pong! Time taken: #{(Time.now - event.timestamp) * 1000} ms."
     end
   end
 end
