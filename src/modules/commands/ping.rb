991e8994b10a3c2e686ed3af130a05df1dbc431e
Connor Shea <connor.james.shea@gmail.com>
2018-06-07 16:50:44 -0600

    Fix the ping command.

--- src/modules/commands/ping.rb
+++ src/modules/commands/ping.rb
@@ -6,8 +6,8 @@ module Bot::DiscordCommands
     command(:ping, description: "Pings the bot, the bot will return a time in milliseconds to show how long the response took.") do |_event|
       # The `respond` method returns a `Message` object, which is stored in a variable `m`. The `edit` method is then called
       # to edit the message with the time difference between when the event was received and after the message was sent.
-      m = event.respond('Pong!')
-      m.edit "Pong! Time taken: #{(Time.now - event.timestamp) * 1000} ms."
+      m = _event.respond('Pong!')
+      m.edit "Pong! Time taken: #{(Time.now - _event.timestamp) * 1000} ms."
     end
   end
 end
