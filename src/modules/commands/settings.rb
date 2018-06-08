4e7f470f234a9f1a9f6947054eab78431f745e4c
Connor Shea <connor.james.shea@gmail.com>
2018-06-07 23:01:20 -0600

    Rename toggle.rb to settings.rb and add a settings command.

--- src/modules/commands/settings.rb
+++ src/modules/commands/settings.rb
@@ -1,8 +1,17 @@
 module Bot::DiscordCommands
   # Responds with "Pong!".
   # This used to check if bot is alive
-  module Toggle
+  module Settings
     extend Discordrb::Commands::CommandContainer
+
+    command(:settings, description: "Shows settings info.") do |event|
+      settings = Bot::Database::Settings.find_or_create(guild_id: event.server.id)
+
+      settings_info = "**Settings**\n"\
+        "`Ouija Mode`: #{settings[:enabled] ? 'Enabled' : 'Disabled'}\n"\
+        "`Delete All Mode`: #{settings[:delete_all] ? 'Enabled' : 'Disabled'}"
+      event.respond(settings_info)
+    end
 
     command(:enable, description: "Enables Ouija mode.") do |event|
       settings = Bot::Database::Settings.find(guild_id: event.server.id)
